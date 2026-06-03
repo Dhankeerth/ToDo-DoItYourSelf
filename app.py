2cba63721f040eab7d3b746210f34e06534feec4
Dhankeerth <asdhankeerth5@gmail.com>
2026-06-03 16:37:21 +0530

    Fixed Render port binding

--- app.py
+++ app.py
@@ -154,7 +154,11 @@ def logout():
     session.pop("user")
     return redirect("/login")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 
 
 
